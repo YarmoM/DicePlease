585810b7ac2c737b82f7da81c271626b26b01607
Yarmo Mackenbach <yarmo@protonmail.com>
2020-01-17 17:20:25 +0100

    Attempt to make search easier

--- search.php
+++ search.php
@@ -4,17 +4,17 @@ include __DIR__ . '/constants.php';
 
 $params = '';
 
-if (isset($_POST['issue_status']) && $_POST['issue_status'] != $DEF_STATUS) {
-    $params .= 'status='.$_POST['issue_status'].'&';
+if (isset($_POST['status']) && $_POST['status'] != $DEF_STATUS) {
+    $params .= 'status='.$_POST['status'].'&';
 }
-if (isset($_POST['issue_type']) && $_POST['issue_type'] != $DEF_TYPE) {
-    $params .= 'type='.$_POST['issue_type'].'&';
+if (isset($_POST['type']) && $_POST['type'] != $DEF_TYPE) {
+    $params .= 'type='.$_POST['type'].'&';
 }
-if (isset($_POST['issue_game']) && $_POST['issue_game'] != $DEF_GAME) {
-    $params .= 'game='.$_POST['issue_game'].'&';
+if (isset($_POST['game']) && $_POST['game'] != $DEF_GAME) {
+    $params .= 'game='.$_POST['game'].'&';
 }
-if (isset($_POST['issue_query']) && $_POST['issue_query'] != $DEF_QUERY) {
-    $params .= 'query='.$_POST['issue_query'].'&';
+if (isset($_POST['query']) && $_POST['query'] != $DEF_QUERY) {
+    $params .= 'query='.$_POST['query'].'&';
 }
 
 if (strlen($params) > 0) {
